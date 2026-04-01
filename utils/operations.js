df3d3691b31b39948c3314ce8b4384c4743e7b0c
Ahmed-Alnasser <s202347950@kfupm.edu.sa>
2026-04-01 08:58:29 +0300

    Todo4: implementing math operations

--- utils/operations.js
+++ utils/operations.js
@@ -1,15 +1,23 @@
+// TODO 4.1
 export function add(numbers) {
-  return {};
+  return numbers.reduce((sum, num) => sum + num, 0);
 }
 
+// TODO 4.2
 export function subtract(numbers) {
-  return {};
+  return numbers.reduce((diff, num, i) => (i === 0 ? num : diff - num), 0);
 }
 
+// TODO 4.3
 export function multiply(numbers) {
-  return {};
+  return numbers.reduce((product, num) => product * num, 1);
 }
 
+// TODO 4.4
 export function divide(numbers) {
-  return {};
+  return numbers.reduce((quotient, num, i) => {
+    if (i === 0) return num;
+    if (num === 0) return NaN;
+    return quotient / num;
+  }, 0);
 }
