67e05879f12cf0fd9fb561117c96d2d7f4aad2cd
Ahmed-Alnasser <s202347950@kfupm.edu.sa>
2026-04-01 09:00:08 +0300

    Todo5: creating parser functions with Lodash

--- utils/parser.js
+++ utils/parser.js
@@ -1,9 +1,13 @@
 import _ from "lodash";
 
+// TODO 5.1
 export function parseNumbers(input) {
-  return {};
+  const numbers = _.map(input, (str) => Number(str));
+  return _.compact(numbers);
 }
 
+// TODO 5.2
 export function isValidOperation(operation) {
-  return {};
+  const validOps = ["add", "subtract", "multiply", "divide"];
+  return _.includes(validOps, operation);
 }
